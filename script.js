a5353148a232c58a7a0a2c8187a6164774de5d61
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2025-03-20 02:19:47 +0000

    This commit is done for level 100 of tv show project by @Mikiyas-STP

--- script.js
+++ script.js
@@ -5,8 +5,23 @@ function setup() {
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  for(let episode of episodeList){
+    let seasonNum = String(episode.season).padStart(2, "0");
+    let episodeNum = String(episode.number).padStart(2, "0");
+    const card = document.createElement("section"); //i created a card section.
+    const movieTitle = document.createElement("h1");
+    const movieImage = document.createElement("img");
+    const movieSummary = document.createElement("p");
+    document.getElementById("root").append(card);
+    movieTitle.textContent = `${episode.name} - S${seasonNum}E${episodeNum}`;
+    card.append(movieTitle);
+    movieImage.src = episode.image.medium;
+    movieImage.alt = "logo of movie";
+    card.append(movieImage);
+    movieSummary.innerHTML = episode.summary; //inner html is used incase of html tags inn the summary.
+    card.append(movieSummary);
+  }
+  
+  
 }
-
 window.onload = setup;
